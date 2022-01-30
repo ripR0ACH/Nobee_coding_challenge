3081c8ce21153295368008f72fd797f89c17d114
zach <ziemboy9@gmail.com>
2022-01-30 01:45:48 -0600

    Updates

---
+++ precache-manifest.3d919ec06b10758131f43560485959a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f64b2d60b8d57c31bbcebb7b880aec7",
+    "url": "/Nobee_coding_challenge/index.html"
+  },
+  {
+    "revision": "701cfc0e86d448486a39",
+    "url": "/Nobee_coding_challenge/static/css/main.0d782a8b.chunk.css"
+  },
+  {
+    "revision": "f92dfe15a964b71f1664",
+    "url": "/Nobee_coding_challenge/static/js/2.8cbe249b.chunk.js"
+  },
+  {
+    "revision": "701cfc0e86d448486a39",
+    "url": "/Nobee_coding_challenge/static/js/main.22b87879.chunk.js"
+  },
+  {
+    "revision": "73746089dc91a1e50a87",
+    "url": "/Nobee_coding_challenge/static/js/runtime~main.9f7946e5.js"
+  }
+]);
